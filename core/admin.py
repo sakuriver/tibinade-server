d921f7b0ef4789cee920fadedb9016e2fd0f7ab6
sakuriver <suzue@yahoo.co.jp>
2016-07-16 20:46:25 +0900

    モデルのコードでエラーが出ていたので修正

--- core/admin.py
+++ core/admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
 from .models import *
-from  rebirthchronicle_server.settings import *
+from  tibinade_server.settings import *
 admin.site.site_header = ADMIN_SITE_TITLE
--- core/models.py
+++ core/models.py
@@ -8,7 +8,7 @@ class ItemMaster(models.Model):
         verbose_name_plural = 'アイテムマスタ'
     id = models.IntegerField('id')
     chara_id = models.IntegerField('キャラマスタID')
-    name = modes.CharField('名前', max_length=255)
+    name = models.CharField('名前', max_length=255)
     image = models.CharField('画像名', max_length=255)
     please_ok_level = models.IntegerField('頼めるレベル')
     please_number = models.IntegerField('頼む回数')
